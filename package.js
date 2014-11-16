7c263961b7fef260327dc48ee0ecfccc8730b63a
Christian Fritz <christian.fritz@gmail.com>
2014-11-15 20:59:25 -0800

    - updated package.js to comply with new meteor package system   standards

--- package.js
+++ package.js
@@ -5,9 +5,10 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use('underscore', 'server');
-  if (api.export) {
-    api.export('Cron','server');
-  }
-  api.add_files('cron.js', ['server']);
+    api.versionsFrom('0.9.0');
+    api.use('underscore', 'server');
+    if (api.export) {
+        api.export('Cron','server');
+    }
+    api.add_files('cron.js', ['server']);
 });
